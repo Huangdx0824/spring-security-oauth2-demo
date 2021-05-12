698f9b231fd074de0724150c88ba4871c3660d5d
zero <1580359334@qq.com>
2021-05-13 00:14:39 +0800

    授权服务器配置

---
+++ src/main/java/com/huang/config/AuthorizationServerConfig.java
@@ -0,0 +1,38 @@
+package com.huang.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+/**
+ * 授权服务器配置
+ */
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                // 配置client-id
+                .withClient("admin")
+                // 配置client-secret
+                .secret(passwordEncoder.encode("123321"))
+                //配置访问token的有效期
+                .accessTokenValiditySeconds(3600)
+                //配置redirect_uri,用于授权成功后跳转
+                .redirectUris("http://www.baidu.com")
+                //配置申请的权限范围
+                .scopes("all")
+                //配置grant_type，标示授权类型
+                .authorizedGrantTypes("authorization_code");
+    }
+}
