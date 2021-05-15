ce5be90e56976e36699ebe7e56a65064a6edc42f
zero <1580359334@qq.com>
2021-05-15 20:35:08 +0800

    解析JWT

--- src/main/java/com/huang/controller/UserController.java
+++ src/main/java/com/huang/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.huang.controller;
 
+import io.jsonwebtoken.Jwts;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/user")
@@ -16,8 +20,12 @@ public class UserController {
      */
 
     @RequestMapping("/getCurrentUser")
-    public Object getCurrentUser(Authentication authentication) {
-        System.out.println(authentication.getPrincipal());
-        return authentication.getPrincipal();
+    public Object getCurrentUser(Authentication authentication, HttpServletRequest request) {
+        String head = request.getHeader("Authorization");
+        String token = head.substring(head.indexOf("bearer") + 7);
+        return Jwts.parser()
+                .setSigningKey("test_key".getBytes(StandardCharsets.UTF_8))
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
