31f8f299e91cdcf635a6d85d865c60f6808bbef0
zero <1580359334@qq.com>
2021-05-15 15:48:04 +0800

    添加密码模式

--- src/main/java/com/huang/config/AuthorizationServerConfig.java
+++ src/main/java/com/huang/config/AuthorizationServerConfig.java
@@ -1,10 +1,15 @@
 package com.huang.config;
 
+import com.huang.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 /**
  * 授权服务器配置
@@ -17,11 +22,28 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableAuthorizationServer //启用授权服务器配置
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
-    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-    public AuthorizationServerConfig(PasswordEncoder passwordEncoder) {
-        this.passwordEncoder = passwordEncoder;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService userService;
+
+
+    /**
+     * 使用密码模式所需配置
+     *
+     * @Author Brian
+     * @Date 2021-05-14
+     */
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager)
+                .userDetailsService(userService);
     }
+
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -31,12 +53,13 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 // 配置client-secret
                 .secret(passwordEncoder.encode("112233"))
                 //配置访问token的有效期
-                .accessTokenValiditySeconds(3600)
+                // .accessTokenValiditySeconds(3600)
                 //配置redirect_uri,用于授权成功后跳转
                 .redirectUris("http://www.baidu.com")
                 //配置申请的权限范围
                 .scopes("all")
                 //配置grant_type，标示授权类型
-                .authorizedGrantTypes("authorization_code");
+                // .authorizedGrantTypes("authorization_code");
+                .authorizedGrantTypes("password");
     }
 }
--- src/main/java/com/huang/config/SecurityConfig.java
+++ src/main/java/com/huang/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.huang.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,10 +19,22 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity // 启用Web安全
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * 需要配置这个支持password模式
+     * support password grant type
+     *
+     * @Author Brian
+     * @Date 2021-05-15
+     */
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Override
